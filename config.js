4c94b2b669d0e0cf0091b1a709d35327eca7d05f
marconeri70 <marconeri70@gmail.com>
2026-07-24 13:18:15 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.BOOKSALE_CONFIG = {
   TURNSTILE_SITE_KEY: "0x4AAAAAAD7JzaUSfwLPH4S2",
 
   // Indirizzo mostrato nelle pagine Privacy e Condizioni d'uso.
-  LEGAL_CONTACT_EMAIL: "marconeri70@gmail.com",
+  LEGAL_CONTACT_EMAIL: "INSERISCI_EMAIL_CONTATTO",
 
   SITE_NAME: "BookSale",
 
